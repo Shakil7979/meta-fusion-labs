5a88cf4646a0e94588e6bd1222d53d76b85ae80c
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2025-05-16 01:05:16 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,83 @@
-$(document).ready(function(){
-	// Mobile Menu
-	// $('.logo a i').click(function(){
-	// 	$('.menu ul').slideToggle(1000);
+ 
+//   tsParticles.load("tsparticles", {
+//     background: {
+//       color: {
+//         value: "#000"
+//       }
+//     },
+//     particles: {
+//       number: {
+//         value: 100
+//       },
+//       color: {
+//         value: "#fdd085"
+//       },
+//       shape: {
+//         type: "circle"
+//       },
+//       opacity: {
+//         value: 0.5
+//       },
+//       size: {
+//         value: 3
+//       },
+//       move: {
+//         enable: true,
+//         speed: 2
+//       }
+//     },
+//     interactivity: {
+//       events: {
+//         onhover: {
+//           enable: true,
+//           mode: "repulse"
+//         }
+//       }
+//     }
+//   }); 
 
-	// 	return false
-	// });
 
-	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
+ $(document).ready(function () {
+    $('.mobile-bars').click(function () {
+      $('.nav').slideToggle();  
+
+      // Toggle icon
+      const icon = $(this).find('i');
+      if (icon.hasClass('fa-bars-staggered')) {
+        icon.removeClass('fa-bars-staggered').addClass('fa-xmark');
+      } else {
+        icon.removeClass('fa-xmark').addClass('fa-bars-staggered');
+      }
+    });
 
 
-
-
-
-});
+	$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fas fa-angle-left"></i>',
+      '<i class="fas fa-angle-right"></i>',
+    ],
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1.4,
+      },
+      667: {
+        items: 2.4,
+        margin: 20,
+      },
+      1000: {
+        items: 3.4,
+        margin: 30,
+      },
+      1300: {
+        items: 4.4,
+        margin: 30,
+      },
+    },
+  });
+  });
